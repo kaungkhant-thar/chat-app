cba2163b4f24375d66ee268a87784f0578f62e7c
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-04-19 16:54:08 +0700

    fix: style

--- apps/web/src/app/(protected)/chat/[id]/page.tsx
+++ apps/web/src/app/(protected)/chat/[id]/page.tsx
@@ -177,7 +177,7 @@ const ChatPage = () => {
     ?.user.id;
 
   return (
-    <div className="flex flex-col h-full max-h-[calc(100vh-80px)]">
+    <div className="flex flex-col h-full max-h-[calc(100vh-120px)] lg:max-h-full">
       {chat ? (
         <>
           <MessageList
