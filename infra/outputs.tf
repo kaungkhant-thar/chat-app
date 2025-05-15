33d858b30acc1ddb029da791f066dec8f79df205
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-05-15 21:10:37 +0700

    fix: port

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -11,3 +11,14 @@ output "web_ecr_url" {
 output "server_ecr_url" {
   value = aws_ecr_repository.server.repository_url
 }
+
+output "web_task_definition_arn" {
+  value = aws_ecs_task_definition.web.arn
+  
+}
+
+
+output "server_task_definition_arn" {
+  value = aws_ecs_task_definition.server.arn
+
+}
