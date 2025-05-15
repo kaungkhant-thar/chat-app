6d14cdcfc48c54a1a44142f0418a6d352a79252b
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-05-15 20:35:44 +0700

    fix: issues

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -8,10 +8,30 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+
+
 }
 
 
 data "aws_security_group" "default" {
   name   = "default"
   vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "allow_http_3000" {
+  type = "ingress"
+  from_port = "3000"
+  to_port = "3000"
+  protocol = "tcp"
+  security_group_id = data.aws_security_group.default.id
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_http_4000" {
+  type = "ingress"
+  from_port = "4000"
+  to_port = "4000"
+  protocol = "tcp"
+  security_group_id = data.aws_security_group.default.id
+  cidr_blocks = ["0.0.0.0/0"]
 }
