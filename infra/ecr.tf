2f7765499b92ac5e12daa79e1b13a99380e9e215
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-05-17 19:52:34 +0700

    fix: issue

--- infra/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "web" {
-  name = var.ecr_web_repo
-}
-
-resource "aws_ecr_repository" "server" {
-  name = var.ecr_server_repo
-  
-}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,3 @@
-variable "aws_region" {
-  default = "ap-southeast-1"
-}
-
-variable "project_name" {
-  default = "chat-app"
-}
-
-variable "ecr_web_repo" {
-  default = "chat-web"
-}
-
-variable "ecr_server_repo" {
-  default = "chat-server"
-}
-
 variable "jwt_secret_value" {
   type        = string
   sensitive   = true
@@ -22,11 +6,4 @@ variable "jwt_secret_value" {
 variable "database_url_value" {
   type        = string
   sensitive   = true
-}
-
-
-variable "security_group_ids" {
-  type        = list(string)
-  description = "Security group(s) for ECS and ALB"
-  default     = []
 }
